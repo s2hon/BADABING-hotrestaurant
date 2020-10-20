d35be5c1b7e812d17942dff7c49f0f221371aa68
Matthew Oballe <oballematt@gmail.com>
2020-10-20 13:56:22 -0500

    adding backend server

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const reservation = [];
+const waitlist = []
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+
+app.get("/api/tables", function(req, res) {
+  return res.json(reservation);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
+
+  app.post("/api/tables", function(req, res){
+      const newReservation = req.body
+     reservation.push(newReservation)
+  })
+
+  app.listen(PORT, function(){
+      console.log("Listening on PORT " + PORT)
+  })
+
+
+
+
+
+
+
+
+app.get("/api/characters/:character", function(req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (var i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newCharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+
+  res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
