3102cd364d1cc7d6483f50043b38b10b70b6400a
Staci Shon <staci.shon@gmail.com>
2020-10-21 03:47:35 -0500

    corrected footer, added server2.js (just for fun we can comment it out

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
@@ -30,6 +30,7 @@ app.get("/api/tables", function(req, res) {
 app.get("/api/waitlist", function(req, res) {
     return res.json(waitlist);
 });
+
 
 app.post("/api/tables", function(req, res){
     const newReservation = req.body;
